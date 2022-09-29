baa797c78cd30e1ae32e57113f3db05be227fb34
romulosilvestre <romulosilvestre@df.estudante.senai.br>
2022-09-29 11:20:48 -0300

    gabarito da serie Start

---
+++ SerieParaMalharStart.Algoritmo13/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieParaMalharStart.Algoritmo13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*O que é media ponderada?*/
+            /*
+             * Operadores Aritméticos
+             * 
+             * float x, y, calculo;
+            calculo = x + y;
+            calculo= x*y;
+            calculo = x / y;*/
+
+            /*
+             * Frameworks
+             * ASP.NET x Spring x JBOSS Seam xEntity Framework
+             * Django
+             * 
+             * Lógica+Proatividade+Vencer o Desafio+Corrigir os erros+OO
+             * 
+             * 20 anos de experiência
+             * técnico
+             *    etb
+             *    delphi
+             *    Java - OO 4 vezes. Desistir. Voltar pra casa.
+             *    America Skills - 3º America Latina
+             *    C# -OO
+             *    1- operador de micro
+             *    2- operador de micro
+             *    
+             *    Implementação múltipla de interface
+             *    Construtor
+             *    Instância
+             *    Variável
+             *    objeto
+             *    
+             *    
+             *    Udemy
+             *      -anota uma pergunta e joga na aula.
+             *    YouTube
+             *      - Pergunta ele sabe ou sabe + ou -
+             *      
+             *      Silêncio Total
+             *      Online- durmindo, shopping,youtube,estudando
+             *      
+             *      *Carro *Casa
+             *      Conhecimento
+             *      1979+     2100+
+             *      Aluno do SENAI - Conhecimento Transferir
+             *      Upload   Download
+             *        
+      
+             *    
+             *
+             *    
+             *    
+             *    
+             * 4 java
+             * 4 C#
+             * Senai
+             * Escola Técnica
+             * Professores - certificado (Rômulo C. Silvestre)
+             * Lógica + Orientaçao
+             *  +  -> apto
+             * 
+             * class Calculo{
+             * 
+             *     public void CalcularMediaPonderada(){
+             *       //Google
+             *       //Matemática Básica 7º
+             *       //Proatividade
+             *       //Desafio
+             *       //Vida
+             *       //Alma
+             *       
+             *       Trabalhar
+             *       Criar classe
+             *          *objetos
+             *          *métodos 
+             *             cálculo (regra de negócio - lógico)
+             *             Produtividade - Framework
+             *     
+             *     }
+             * }
+             * 
+             */
+            //O mundo agora esta dominado pelos mais rápidos.
+
+            double raiz = Math.Sqrt(25);
+            Console.WriteLine($"A raiz é:{raiz}");
+            double PI = Math.PI;
+            Console.WriteLine($"Valor de PI{PI}");
+            double quadrado = Math.Pow(5, 2);
+            Console.WriteLine($"Valor do quadrado:{quadrado}");
+            double cubico = Math.Pow(3, 3);
+            Console.WriteLine($"Valor ao cubo:{cubico}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SerieParaMalharStart.Alongamento/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieParaMalharStart.Alongamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comentário de uma linha
+            //Alongamento
+            //Variável - Atribuição - Entrada e Saída
+            //Funções para String
+
+            /*
+             * Comentários de múltiplas linhas
+             */
+            Console.WriteLine("Digite o nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a média");
+            double media = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a idade");
+            int idade = int.Parse(Console.ReadLine());
+            string mensagem = idade < 18 ? "mEnoR" : "mAiOr";//erro lógico
+            Console.WriteLine($"{nome} sua média foi {media}");
+            Console.WriteLine($"{nome} você é  {mensagem.ToUpper()}");
+            Console.ReadKey();
+        }
+    }
+}
